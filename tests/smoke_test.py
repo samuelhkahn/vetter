d38ca188b590e8a7fd16846b37cab0d63696df99
Samuel Kahn <samuelkahn@berkeley.edu>
2026-09-07 18:26:28 -0700

    fixing comment space

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -1,6 +1,4 @@
 def test_smoke():
     assert True
-
-
-def test_smoke_two_raise_ruff(): # removed, just for testing ruff
+def test_smoke_two_raise_ruff():  # removed, just for testing ruff
     assert True
