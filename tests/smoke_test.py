d57d20d5cac606291e2bf4835e6066221765caf9
Samuel Kahn <samuelkahn@berkeley.edu>
2026-09-07 18:19:09 -0700

    CI test

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -2,6 +2,5 @@ def test_smoke():
     assert True
 
 
-# def test_smoke_two_raise_ruff(): # removed, just for testing ruff
-#     if 2>1:
-#         assert True
+def test_smoke_two_raise_ruff(): # removed, just for testing ruff
+        assert False
